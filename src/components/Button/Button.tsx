0563a3641bb4a9984191d0ff7f027a5e61d41ecc
khamudom <khamu@outlook.com>
2023-05-23 20:14:19 -0700

    update destiny images and add more attribute to Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,15 +5,27 @@ type ButtonProps = {
   children: React.ReactNode;
   className?: string;
   onClick?: () => void;
+  type?: 'button' | 'submit' | 'reset';
+  title?: string;
+  ref?: React.RefObject<HTMLButtonElement>;
 };
 
 const Button: React.FC<ButtonProps> = ({
   children,
   className = '',
   onClick,
+  type,
+  title,
+  ref,
 }) => {
   return (
-    <button className={`${styles.container} ${className}`} onClick={onClick}>
+    <button
+      className={`${styles.container} ${className}`}
+      onClick={onClick}
+      type={type}
+      title={title}
+      ref={ref}
+    >
       {children}
     </button>
   );
